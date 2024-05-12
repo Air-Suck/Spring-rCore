f052ce9294eb47043da7599790df2602482e3235
snh <2640327187@qq.com>
2024-05-12 22:35:48 +0800

    ch3

---
+++ ch3/os/target/release/deps/rustc_version-d800bab910800201.d
@@ -0,0 +1,8 @@
+/home/shinuohui/code/2024s-rcore-Air-Suck/os/target/release/deps/librustc_version-d800bab910800201.rmeta: /home/shinuohui/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/rustc_version-0.2.3/src/lib.rs /home/shinuohui/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/rustc_version-0.2.3/src/errors.rs
+
+/home/shinuohui/code/2024s-rcore-Air-Suck/os/target/release/deps/librustc_version-d800bab910800201.rlib: /home/shinuohui/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/rustc_version-0.2.3/src/lib.rs /home/shinuohui/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/rustc_version-0.2.3/src/errors.rs
+
+/home/shinuohui/code/2024s-rcore-Air-Suck/os/target/release/deps/rustc_version-d800bab910800201.d: /home/shinuohui/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/rustc_version-0.2.3/src/lib.rs /home/shinuohui/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/rustc_version-0.2.3/src/errors.rs
+
+/home/shinuohui/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/rustc_version-0.2.3/src/lib.rs:
+/home/shinuohui/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/rustc_version-0.2.3/src/errors.rs:
