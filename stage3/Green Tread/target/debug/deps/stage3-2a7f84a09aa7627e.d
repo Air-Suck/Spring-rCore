c2189f1b7ef9a0e237497b4208ffb21d15d03282
snh <2640327187@qq.com>
2024-05-31 10:58:57 +0800

    new stage3 branch

---
+++ stage3/Green Tread/target/debug/deps/stage3-2a7f84a09aa7627e.d
@@ -0,0 +1,5 @@
+/home/shinuohui/code/Spring-rCore/stage3/Green Tread/target/debug/deps/libstage3-2a7f84a09aa7627e.rmeta: src/main.rs
+
+/home/shinuohui/code/Spring-rCore/stage3/Green Tread/target/debug/deps/stage3-2a7f84a09aa7627e.d: src/main.rs
+
+src/main.rs:
---
+++ stage3/fat pointer/target/debug/deps/stage3-2a7f84a09aa7627e.d
@@ -0,0 +1,5 @@
+/home/shinuohui/code/Spring-rCore/stage3/fat pointer/target/debug/deps/libstage3-2a7f84a09aa7627e.rmeta: src/main.rs
+
+/home/shinuohui/code/Spring-rCore/stage3/fat pointer/target/debug/deps/stage3-2a7f84a09aa7627e.d: src/main.rs
+
+src/main.rs:
